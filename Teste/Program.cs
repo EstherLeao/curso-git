65ba3b7c8a9c907ca3bee1f8c2f66346e98c9897
Esther Leão <esther.biasantos@gmail.com>
2020-03-11 18:08:05 -0300

    Acrescentei mensagem de Bom dia

--- Teste/Program.cs
+++ Teste/Program.cs
@@ -6,7 +6,10 @@ namespace Teste
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Olá mundo!");
+            Console.WriteLine("Bom dia!");
+
+            Console.ReadLine();
         }
     }
 }
